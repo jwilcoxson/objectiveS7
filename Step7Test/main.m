ce1fbc2fa12f445dacdeb26575f874f924ac82b4
Joe Wilcoxson <joe.wilcoxson@gmail.com>
2016-05-17 11:03:24 -0400

    Initial Commit

---
+++ Step7Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Step7Test
+//
+//  Created by Joe Wilcoxson on 5/17/16.
+//  Copyright © 2016 Joe Wilcoxson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
